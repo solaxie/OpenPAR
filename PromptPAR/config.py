d1b9188562fc98692b4b77120fca47e361298917
sola xie <sola.xie@gmail.com>
2024-10-06 15:56:54 +0800

    modify --image

--- PromptPAR/config.py
+++ PromptPAR/config.py
@@ -46,6 +46,6 @@ def argument_parser():
     parser.add_argument("--checkpoint", action='store_true')
     parser.add_argument("--dir", type=str, default=None)
     
-    parser.add_argument("--external_image", type=str, help="Path to external image for inference")
+    parser.add_argument("--image", type=str, help="Path to external image for inference")
     
     return parser
